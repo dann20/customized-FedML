d29025292697f3046fc1ec3b36d0af4ab3923037
dann20 <nmdan0611@gmail.com>
2021-04-03 20:04:29 +0700

    Modification commit

--- FedML-Server/executor/VAE-LSTM-app.py
+++ FedML-Server/executor/VAE-LSTM-app.py
@@ -124,4 +124,4 @@ if __name__ == '__main__':
     server_manager.send_init_vae_msg()
 
     # if run in debug mode, process will be single threaded by default
-    app.run(host='192.168.4.5', port=5000)
+    app.run(host='192.168.1.10', port=5000)
