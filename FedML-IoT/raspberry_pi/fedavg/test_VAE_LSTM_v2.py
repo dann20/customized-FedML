e1513c43372e293e41ed36d2b120c7a64264840c
dann20 <nmdan0611@gmail.com>
2021-06-28 13:07:00 +0700

    Add test script

--- FedML-IoT/raspberry_pi/fedavg/test_VAE_LSTM_v2.py
+++ FedML-IoT/raspberry_pi/fedavg/test_VAE_LSTM_v2.py
@@ -20,11 +20,8 @@ import time
 import os
 import sys
 import subprocess
-
-tf.compat.v1.disable_eager_execution()
-sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../")))
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+import math
+from scipy.stats import norm
 
 import tensorflow as tf
 from FedML.fedml_api.data_preprocessing.VAE_LSTM.data_loader import DataGenerator
@@ -33,61 +30,10 @@ from FedML.fedml_api.distributed.fedavg.VAE_Trainer import vaeTrainer
 
 from FedML.fedml_api.distributed.fedavg.utils_VAE_LSTM import process_config, create_dirs, get_args, save_config
 
-from tensorflow.python.client import device_lib
-
-def get_available_gpus():
-    local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos if x.device_type == 'GPU']
-
-print(get_available_gpus())
-
-start = time.time()
-
-# load VAE model
-config = process_config('../VAE-LSTM-related/configs/scada1_config.json')
-# create the experiments dirs
-create_dirs([config['result_dir'], config['checkpoint_dir']])
-# create tensorflow session
-sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
-# create your data generator
-
-data = DataGenerator(config,3)
-# create a CNN model
-model_vae = VAEmodel(config, "Global")
-# create a CNN model
-trainer_vae = vaeTrainer(sess, model_vae, data, config)
-model_vae.load(sess)
-
-# load LSTM model
-lstm_model = lstmKerasModel("Global", config)
-lstm_model.produce_embeddings(model_vae, data, sess)
-lstm_nn_model = lstm_model.create_lstm_model()
-lstm_nn_model.summary()   # Display the model's architecture
-
-# checkpoint path
-checkpoint_path = config['checkpoint_dir_lstm'] + "cp_Global.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
-# Create a callback that saves the model's weights
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
-# load weights if possible
-lstm_model.load_model(lstm_nn_model, checkpoint_path)
-
-# load normalised time series
-save_dir = '../VAE-LSTM-related/datasets/NAB-known-anomaly/'
-# config['dataset'] = 'ecg_3'
-dataset = config['dataset']
-filename = '{}.npz'.format(dataset)
-result = dict(np.load(save_dir+filename, allow_pickle=True))
-if dataset == 'machine_temp':
-    result['test'] = result['test'][0]
-    result['idx_anomaly_test'] = result['idx_anomaly_test'][0]
-    result['t_test'] = result['t_test'][0]
-
-print("------START-TESTING-------")
-start_testing=time.time()
-resmon_process = subprocess.Popen(["resmon", "-o", "resmon_scada1_testing_1.csv"])
+tf.compat.v1.disable_eager_execution()
+sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../")))
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 # slice into rolling windows and rolling sequences
 def slice_rolling_windows_and_sequences(config, time_seq):
@@ -114,13 +60,6 @@ def slice_rolling_windows_and_sequences(config, time_seq):
     #    for j in range(config['l_seq']):
     #        lstm_seq[i,j] = cur_seq[config['l_win']*j:config['l_win']*(j+1)]
     return rolling_windows, lstm_seq, sample_m, sample_std
-
-result['test'] = result['test'].astype(np.float32)
-test_windows, test_seq, test_sample_m, test_sample_std = slice_rolling_windows_and_sequences(config, result['test'])
-test_windows = np.reshape(test_windows, (-1,config['l_win'],config['n_channel']))
-test_seq = np.reshape(test_seq, (-1,config['l_seq'],config['l_win'],config['n_channel']))
-print('test_windows ',test_windows.shape)
-print('test_seq ',test_seq.shape)
 
 # Evaluate ELBO and LSTM prediction error on the validation set
 # evaluate some anomaly detection metrics
@@ -165,59 +104,6 @@ def evaluate_lstm_anomaly_metric_for_a_seq(test_seq):
     lstm_recons_error = np.sum(np.square(recons_win_lstm - np.squeeze(test_seq[1:])))
     error_original = np.abs(recons_win_lstm - np.squeeze(test_seq[1:])).reshape((config['l_seq']-1,-1)) #them dong nay de tinh
     return lstm_recons_error, lstm_embedding_error, error_original
-data.val_set_vae['data'] = np.asarray(data.val_set_vae['data']).astype(np.float32)
-data.val_set_lstm['data'] = np.asarray(data.val_set_lstm['data']).astype(np.float32)
-data.train_set_lstm['data'] = np.asarray(data.train_set_lstm['data']).astype(np.float32)
-# data.train_set_vae['data'] = np.asarray(data.train_set_vae['data']).astype(np.float32)
-n_val_vae = data.val_set_vae['data'].shape[0]
-n_val_lstm = data.val_set_lstm['data'].shape[0]
-
-val_vae_recons_error = np.zeros(n_val_vae, dtype=np.float32)
-val_vae_kl_error = np.zeros(n_val_vae, dtype=np.float32)
-val_vae_elbo_loss = np.zeros(n_val_vae, dtype=np.float32)
-for i in range(n_val_vae):
-    val_vae_recons_error[i], val_vae_kl_error[i], val_vae_elbo_loss[i] = evaluate_vae_anomaly_metrics_for_a_window(data.val_set_vae['data'][i])
-
-val_lstm_recons_error, val_lstm_embedding_error = np.zeros(n_val_lstm, dtype=np.float32), np.zeros(n_val_lstm, dtype=np.float32)
-val_lstm_error_original = np.zeros((n_val_lstm,config['l_seq']-1,config['l_win']*config['n_channel']), dtype=np.float32)
-for i in range(n_val_lstm):
-    val_lstm_recons_error[i], val_lstm_embedding_error[i], val_lstm_error_original[i] = evaluate_lstm_anomaly_metric_for_a_seq(data.val_set_lstm['data'][i])
-
-n_train_lstm = data.train_set_lstm['data'].shape[0]
-train_lstm_recons_error, train_lstm_embedding_error = np.zeros(n_train_lstm, dtype=np.float32), np.zeros(n_train_lstm, dtype=np.float32)
-train_lstm_error_original = np.zeros((n_train_lstm,config['l_seq']-1,config['l_win']*config['n_channel']), dtype=np.float32) #them de tinh OCSVM
-for i in range(n_train_lstm):
-    train_lstm_recons_error[i], train_lstm_embedding_error[i], train_lstm_error_original[i] = evaluate_lstm_anomaly_metric_for_a_seq(data.train_set_lstm['data'][i])
-
-# Evaluate the anomaly metrics on the test windows and sequences
-n_test_lstm = test_seq.shape[0]
-
-test_lstm_recons_error, test_lstm_embedding_error = np.zeros(n_test_lstm, dtype=np.float32), np.zeros(n_test_lstm, dtype=np.float32)
-test_lstm_error_original = np.zeros((n_test_lstm,config['l_seq']-1,config['l_win']*config['n_channel']), dtype=np.float32)
-for i in range(n_test_lstm):
-    test_lstm_recons_error[i], test_lstm_embedding_error[i], test_lstm_error_original[i] = evaluate_lstm_anomaly_metric_for_a_seq(test_seq[i])
-print("All windows' reconstruction error is computed.")
-print("The total number of windows is {}".format(len(test_lstm_recons_error)))
-
-# Produce the ground truth anomaly indices
-# if result['idx_split'][0] == 0:
-#     idx_anomaly_test = result['idx_anomaly_test']
-# else:
-#     idx_anomaly_test = result['idx_anomaly_test'][0]
-idx_anomaly_test = result['idx_anomaly_test']
-anomaly_index_lstm = []
-test_labels_lstm = np.zeros(n_test_lstm, dtype=np.float32)
-for i in range(len(idx_anomaly_test)):
-    idx_start = idx_anomaly_test[i]-(config['l_win']*config['l_seq']-1)
-    idx_end = idx_anomaly_test[i]+1
-    if idx_start < 0:
-        idx_start = 0
-    if idx_end > n_test_lstm:
-        idx_end = n_test_lstm
-    anomaly_index_lstm.append(np.arange(idx_start,idx_end))
-    test_labels_lstm[idx_start:idx_end] = 1
-
-print(test_labels_lstm.shape)
 
 def return_anomaly_idx_by_threshold(test_anomaly_metric, threshold):
     # test_list = np.squeeze(np.ndarray.flatten(test_anomaly_metric))
@@ -243,156 +129,99 @@ def augment_detected_idx(idx_detected_anomaly, anomaly_index):
 
     return list(np.sort(idx_detected_anomaly_extended))
 
-def count_TP_FP_FN(idx_detected_anomaly, anomaly_index, test_labels):
-    n_TP = 0
-    n_FP = 0
-    #n_detection = len(idx_detected_anomaly)
-    #for i in range(n_detection):
-    for i in idx_detected_anomaly:
-        #if test_labels[idx_detected_anomaly[i]] == 1:
-        if test_labels[i] == 1:
-            n_TP = n_TP + 1
-        else:
-            n_FP = n_FP + 1 #both branch the same?
 
-    idx_undetected = list(set(np.arange(len(test_labels)))- set(idx_detected_anomaly))
-    n_FN = 0
-    for i in idx_undetected:
-        if test_labels[i] == 1:
-            n_FN = n_FN + 1
-    n_TN = len(test_labels) - n_TP - n_FP - n_FN
-    return n_TP, n_FP, n_FN, n_TN
+start = time.time()
 
-def compute_precision_and_recall(idx_detected_anomaly, anomaly_index, test_labels):
-    # compute true positive
-    n_TP, n_FP, n_FN, n_TN = count_TP_FP_FN(idx_detected_anomaly, anomaly_index, test_labels)
+# load VAE model
+config = process_config('../VAE-LSTM-related/configs/scada1_config.json')
+# create the experiments dirs
+create_dirs([config['result_dir'], config['checkpoint_dir']])
+# create tensorflow session
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
+# create a CNN model
+model_vae = VAEmodel(config, "Global")
+# create a CNN model
+model_vae.load(sess)
 
-    if n_TP + n_FP == 0:
-        precision = 1
-    else:
-        precision = n_TP / (n_TP + n_FP)
-    recall = n_TP / (n_TP + n_FN)
-    if precision + recall == 0:
-        F1 = 0
-    else:
-        F1 = 2* (precision * recall)/(precision + recall)
-    fpr = n_FP/(n_FP + n_TN)
+# load LSTM model
+lstm_model = lstmKerasModel("Global", config)
+lstm_nn_model = lstm_model.create_lstm_model()
+lstm_nn_model.summary()   # Display the model's architecture
 
-    return precision, recall, F1, fpr, n_TP, n_FP, n_FN
+# checkpoint path
+checkpoint_path = config['checkpoint_dir_lstm'] + "cp_Global.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+# load weights if possible
+lstm_model.load_model(lstm_nn_model, checkpoint_path)
 
-n_threshold = 25
-precision = np.zeros(n_threshold, dtype=np.float32)
-recall = np.zeros(n_threshold, dtype=np.float32)
-F1 = np.zeros(n_threshold, dtype=np.float32)
-precision_aug = np.zeros(n_threshold, dtype=np.float32)
-recall_aug = np.zeros(n_threshold, dtype=np.float32)
-F1_aug = np.zeros(n_threshold, dtype=np.float32)
-fpr_aug = np.zeros(n_threshold, dtype=np.float32)
-i = 0
-threshold_list = np.linspace(np.amin(test_lstm_recons_error), np.amax(test_lstm_recons_error), n_threshold, endpoint=True)
-threshold_list = np.flip(threshold_list)
-for threshold in threshold_list:
-    #print(threshold_list[i])
-    idx_detection_lstm = return_anomaly_idx_by_threshold(test_lstm_recons_error, threshold)
-    precision[i], recall[i], F1[i], _, _, _, _ = compute_precision_and_recall(idx_detection_lstm,
-                                                                           anomaly_index_lstm,
-                                                                           test_labels_lstm)
-    # augment the detection using the ground truth labels
-    # a method to discount the factor one anomaly appears in multiple consecutive windows
-    # introduced in "Unsupervised anomaly detection via variational auto-encoder for seasonal kpis in web applications"
-    idx_detection_lstm_augmented = augment_detected_idx(idx_detection_lstm, anomaly_index_lstm)
-    precision_aug[i], recall_aug[i], F1_aug[i], fpr_aug[i], _, _, _ = compute_precision_and_recall(idx_detection_lstm_augmented,
-                                                                                       anomaly_index_lstm,
-                                                                                       test_labels_lstm)
-    i = i + 1
-    #print(precision, recall, F1)
+# load normalised time series
+save_dir = '../VAE-LSTM-related/datasets/NAB-known-anomaly/'
+# config['dataset'] = 'ecg_3'
+dataset = config['dataset']
+filename = '{}.npz'.format(dataset)
+result = dict(np.load(save_dir+filename, allow_pickle=True))
+result['test'] = result['test'].astype(np.float32)
+test_data = np.split(result['test'], [10000, 20000, 30000, 40000, 50000, 60000])
 
-print('threshold list:',threshold_list)
+if dataset == 'machine_temp':
+    result['test'] = result['test'][0]
+    result['idx_anomaly_test'] = result['idx_anomaly_test'][0]
+    result['t_test'] = result['t_test'][0]
 
-print("Best F1 score is {}".format(np.amax(F1)))
-idx_best_threshold = np.squeeze(np.argwhere(F1 == np.amax(F1)))
-print("Best threshold is {}".format(threshold_list[idx_best_threshold]))
-print("At this threshold, precision is {}, recall is {}".format(precision[idx_best_threshold], recall[idx_best_threshold]))
-average_precision = np.sum(precision[1:] * (recall[1:] - recall[:-1]))
-print("Average precision is {}".format(average_precision))
+def main_func(test_set):
+    test_windows, test_seq, test_sample_m, test_sample_std = slice_rolling_windows_and_sequences(config, test_set)
+    test_windows = np.reshape(test_windows, (-1,config['l_win'],config['n_channel']))
+    test_seq = np.reshape(test_seq, (-1,config['l_seq'],config['l_win'],config['n_channel']))
+    print('test_windows ',test_windows.shape)
+    print('test_seq ',test_seq.shape)
 
-print("\nAugmented detection:")
-print("Best F1 score is {}".format(np.amax(F1_aug)))
-idx_best_threshold = np.squeeze(np.argwhere(F1_aug == np.amax(F1_aug)))
-print("Best threshold is {}".format(threshold_list[idx_best_threshold]))
-best_thres = np.min(threshold_list[idx_best_threshold])
-print("At this threshold, precision is {}, recall is {}".format(precision_aug[idx_best_threshold],
-                                                                recall_aug[idx_best_threshold]))
+    # Evaluate the anomaly metrics on the test windows and sequences
+    n_test_lstm = test_seq.shape[0]
 
-average_precision_aug = np.sum(precision_aug[1:] * (recall_aug[1:] - recall_aug[:-1]))
-print("Average precision is {}".format(average_precision_aug))
+    test_lstm_recons_error, test_lstm_embedding_error = np.zeros(n_test_lstm, dtype=np.float32), np.zeros(n_test_lstm, dtype=np.float32)
+    test_lstm_error_original = np.zeros((n_test_lstm,config['l_seq']-1,config['l_win']*config['n_channel']), dtype=np.float32)
+    for i in range(n_test_lstm):
+        test_lstm_recons_error[i], test_lstm_embedding_error[i], test_lstm_error_original[i] = evaluate_lstm_anomaly_metric_for_a_seq(test_seq[i])
+    print("All windows' reconstruction error is computed.")
+    print("The total number of windows is {}".format(len(test_lstm_recons_error)))
 
-import math
-from scipy.stats import norm
+    idx_anomaly_test = result['idx_anomaly_test']
+    anomaly_index_lstm = []
+    test_labels_lstm = np.zeros(n_test_lstm, dtype=np.float32)
+    for i in range(len(idx_anomaly_test)):
+        idx_start = idx_anomaly_test[i]-(config['l_win']*config['l_seq']-1)
+        idx_end = idx_anomaly_test[i]+1
+        if idx_start < 0:
+            idx_start = 0
+        if idx_end > n_test_lstm:
+            idx_end = n_test_lstm
+        anomaly_index_lstm.append(np.arange(idx_start,idx_end))
+        test_labels_lstm[idx_start:idx_end] = 1
 
-def KQp(data,q):
-#UNTITLED3 Summary of this function goes here
-#   Detailed explanation goes here
-    data2 = np.sort(data) #sap xep tang dan
-    n = np.shape(data2)[0] #kich thuoc
-    p = 1-q #q tu xet, dat bang smth 0.05 0.025 0.01
-    h = math.sqrt((p*q)/(n+1))
-    KQ=0
-    for i in range(1,n+1):
-        a= ((i/n)-p)/h
-        b= (((i-1)/n)-p)/h
-        TP=(norm.cdf(a)-norm.cdf(b))*data2[i-1] #normcdf thu trong matlab
-        KQ=KQ+TP
-    #KQp = KQ
-    return KQ
+    print(test_labels_lstm.shape)
 
-for i in [0.3, 0.5, 0.7]:
-    print("KQp is:", KQp(test_lstm_recons_error,i),"with q =",i)
 
-# Now select a threshold
-threshold = best_thres
-q_list = [0.99, 0.95, 0.9, 0.85, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
-temp = math.inf
-q_best = 0
-closest_thres = 0
-for q in q_list:
-    temp_thres = KQp(test_lstm_recons_error, q)
-    #print(temp_thres,abs(temp_thres - threshold))
-    if abs(temp_thres - threshold) < temp:
-        temp = abs(temp_thres - threshold)
-        q_best = q
-        KQp_thres = temp_thres
+    # Now select a threshold
+    threshold = 1000
+    idx_detection = return_anomaly_idx_by_threshold(test_lstm_recons_error, threshold)
+    # print(idx_detection)
+    idx_detection_augmented = augment_detected_idx(idx_detection, anomaly_index_lstm)
+    #print(anomaly_index_lstm)
+    print(idx_detection_augmented)
 
-print("Closest KQp threshold is {} at q = {}".format(KQp_thres,q_best))
-idx_detection = return_anomaly_idx_by_threshold(test_lstm_recons_error, KQp_thres)
-#print(idx_detection)
-idx_detection_augmented = augment_detected_idx(idx_detection, anomaly_index_lstm)
-#print(anomaly_index_lstm)
-#print(idx_detection_augmented)
-precision, recall, F1, _, n_TP, n_FP, n_FN = compute_precision_and_recall(idx_detection_augmented,
-                                                                       anomaly_index_lstm,
-                                                                       test_labels_lstm)
-print("\nPR evaluation using KQE:")
-print("Precision: {}".format(precision))
-print("Recall: {}".format(recall))
-print("F1: {}".format(F1))
-print("TP: {}".format(n_TP))
-print("FP: {}".format(n_FP))
-print("FN: {}".format(n_FN))
+    print("\nThreshold is {}".format(threshold))
+    idx_detection = return_anomaly_idx_by_threshold(test_lstm_recons_error, threshold)
+    idx_detection_augmented = augment_detected_idx(idx_detection, anomaly_index_lstm)
 
-print("\nThreshold is {}".format(threshold))
-idx_detection = return_anomaly_idx_by_threshold(test_lstm_recons_error, threshold)
-idx_detection_augmented = augment_detected_idx(idx_detection, anomaly_index_lstm)
-precision, recall, F1, _,n_TP, n_FP, n_FN = compute_precision_and_recall(idx_detection_augmented,
-                                                                       anomaly_index_lstm,
-                                                                       test_labels_lstm)
-print("\nPR evaluation using augmented detection:")
-print("Precision: {}".format(precision))
-print("Recall: {}".format(recall))
-print("F1: {}".format(F1))
-print("TP: {}".format(n_TP))
-print("FP: {}".format(n_FP))
-
+print("------START-TESTING-------")
+start_testing=time.time()
+resmon_process = subprocess.Popen(["resmon", "-o", "resmon_scada1_testing_1.csv"])
+for i in range(len(test_data)):
+    main_func(test_data[i])
 resmon_process.terminate()
 print("------END-TESTING-------")
 end = time.time()
