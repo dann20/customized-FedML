5b9289525c0f42d0ee7c7fb4d927ea3446fbd989
dann20 <nmdan0611@gmail.com>
2021-12-11 11:03:53 +0700

    Clean repo

--- FedML-Server/executor/server_start.sh
+++
@@ -1,3 +0,0 @@
-# run this script before starting app.py with desired device number and communication rounds
-cd preprocessed_dataset
-python ../../FedML/fedml_api/data_preprocessing/MNIST/mnist_mobile_preprocessor.py --client_num_per_round 2 --comm_round 200
